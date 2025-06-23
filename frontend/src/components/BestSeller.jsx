1c764f7e0203cf357b0894e935a7e751ac4edf11
Anuda Ransara <anudaransara1@gmail.com>
2025-06-23 21:54:09 +0530

    sole best seler bug.

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -9,10 +9,9 @@ function BestSeller() {
     const [bestSeller, setBestSeller] = useState([]);
 
     useEffect(() => {
-         console.log("products", products);
-        const bestProduct = products.filter((item) => item.bestSeller);
+        const bestProduct = products.filter((item) => item.bestseller);
         setBestSeller(bestProduct.slice(0, 5));
-    }, [products]);
+    }, []);
 
     return (
         <div className='my-10'>
