c07d21854cf5af725450f586ea8160657abfa952
Anuda Ransara <anudaransara1@gmail.com>
2025-07-03 23:54:59 +0530

    create add to cart logick.

--- frontend/src/context/shopContext.jsx
+++ frontend/src/context/shopContext.jsx
@@ -1,6 +1,6 @@
 //commen varibles and state variable
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products} from '../assets/assets';
 
 export const ShoopContext = createContext();
@@ -11,9 +11,34 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState("");
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItem, setCartItem] = useState({})
+
+    const addToCart = async (itemId, size) => {
+        let cartData = structuredClone(cartItem)
+
+        if (cartData[itemId]){
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1
+            }
+            else{
+                cartData[itemId][size] = 1
+            }
+
+        }
+        else{
+            cartData[itemId] = {}
+            cartData[itemId][size] = 1
+        }
+        setCartItem(cartData)
+    }
+
+    useEffect(()=>{
+        console.log(cartItem);
+        
+    },[cartItem])
 
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItem, addToCart,
     }
 
     return (
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -8,7 +8,7 @@ const Product = () => {
 
   const { productId } = useParams()
   //console.log(productId);
-  const { products, currency } = useContext(ShoopContext)
+  const { products, currency, addToCart } = useContext(ShoopContext)
   const [productData, setProductData] = useState(false)
   const [image, setImage] = useState('')
   const [size, setSize] = useState('')
@@ -69,7 +69,7 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer'>ADD TO CART</button>
+          <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer'>ADD TO CART</button>
           <hr className='mt-8 sm:w-4/5 border-gray-300' />
           <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
             <p>100% Original product.</p>
