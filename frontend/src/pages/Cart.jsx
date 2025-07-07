6b000f3a05864508eabc70b2eb0866305b9004bf
Anuda Ransara <anudaransara1@gmail.com>
2025-07-07 23:53:56 +0530

    create cart data logig.

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShoopContext } from '../context/shopContext'
 
 function Cart() {
+
+  const {products, currency, cartItem} = useContext(ShoopContext)
+  const [cartData, setCartData] = useState([])
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItem){
+      for (const item in cartItem[items]){
+        if (cartItem[items][item]){
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItem[items][item]
+          })
+        }
+      }
+      //console.log(tempData);
+      setCartData(tempData)
+    }
+  },[cartItem])
+
   return (
     <div>
-      Cart
+      
     </div>
   )
 }
