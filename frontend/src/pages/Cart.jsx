fed4aa33ecc5146a7b83c9ca3c3e9986dd98343f
Anuda Ransara <anudaransara1@gmail.com>
2025-07-08 23:59:16 +0530

    add cart data ui

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,16 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShoopContext } from '../context/shopContext'
+import Title from '../components/Title'
+
 
 function Cart() {
 
-  const {products, currency, cartItem} = useContext(ShoopContext)
+  const { products, currency, cartItem } = useContext(ShoopContext)
   const [cartData, setCartData] = useState([])
 
   useEffect(() => {
     const tempData = [];
-    for (const items in cartItem){
-      for (const item in cartItem[items]){
-        if (cartItem[items][item]){
+    for (const items in cartItem) {
+      for (const item in cartItem[items]) {
+        if (cartItem[items][item]) {
           tempData.push({
             _id: items,
             size: item,
@@ -21,11 +23,31 @@ function Cart() {
       //console.log(tempData);
       setCartData(tempData)
     }
-  },[cartItem])
+  }, [cartItem])
 
   return (
-    <div>
-      
+    <div className='border-t pt-14'>
+      <div className='text-2xl mb-3'>
+        <Title text1={'YOUR'} text2={'CART'} />
+      </div>
+      <div>
+        {
+          cartData.map((item, index) =>{
+            const productData = products.find((product) => product._id === item._id)
+
+            return (
+              <div key={index} className='py-4 border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-col-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div className='flex items-start gap-6'>
+                  <img className='w-16 sm:w-20' src={productData.image[0]} alt='' />
+                  <div>
+                    <p className='text-xs sm:text-lg font-medium'> {productData.name} </p>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
