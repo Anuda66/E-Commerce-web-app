5afda57c1d5ebf2e6a5381e090e448a3a0c57a7d
Anuda Ransara <anudaransara1@gmail.com>
2025-07-11 23:07:04 +0530

    create total amount context.

---
+++ frontend/src/components/CartTotal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CartTotal() {
+
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CartTotal
--- frontend/src/context/shopContext.jsx
+++ frontend/src/context/shopContext.jsx
@@ -15,7 +15,6 @@ const ShopContextProvider = (props) => {
     const [cartItem, setCartItem] = useState({})
 
     
-
     const addToCart = async (itemId, size) => {
 
         if(!size) {
@@ -61,6 +60,24 @@ const ShopContextProvider = (props) => {
         
     // },[cartItem])
 
+    const cartAmount = async() => {
+        let totalAmount = 0;
+        for (const items in cartItem){
+            let itemInfo = products.find((product) => product._id === items)
+            for (const item in cartItem[items] ){
+                try{
+                    if (cartItem[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItem[items][item]
+                    }
+                }
+                catch (error){
+
+                }
+            }
+        }
+        return totalAmount;
+    }
+
 
     const updateQuantity = async (itemId, size, quantity) => {
         let cartData = structuredClone(cartItem)
@@ -73,7 +90,7 @@ const ShopContextProvider = (props) => {
 
     const value = {
         products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItem, addToCart, getCartCount,
-        updateQuantity,
+        updateQuantity, cartAmount, 
     }
 
     return (
