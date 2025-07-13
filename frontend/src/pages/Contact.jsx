a03e77940229aef994017a58247d249b8979bb0f
Anuda Ransara <anudaransara1@gmail.com>
2025-07-14 00:48:00 +0530

    create contact us page.

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewLetterBox from '../components/NewLetterBox'
 
 function Contact() {
   return (
     <div>
-      Contact
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={"CONTACT"} text2={'US'} />
+      </div>
+      <div className='mt-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px] ' src={assets.contact_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600 '>Our Store</p>
+          <p className='text-base text-gray-500'>54709 Willms Station Suite 350,<br /> Washington, USA</p>
+          <p className='text-base text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@forever.com</p>
+          <p className='font-semibold text-xl text-gray-600 '>Careers at Forever</p>
+          <p className='text-base text-gray-500'>Learn more about our teams and job openings.</p>
+           <button className='hover:bg-black hover:text-white text-gray-800  px-8 py-4 mt-4 cursor-pointer active:bg-gray-700 border border-gray-700 text-sm '>Explore Jobs</button>
+        </div>
+       
+      </div>
+      <NewLetterBox/>
     </div>
   )
 }
