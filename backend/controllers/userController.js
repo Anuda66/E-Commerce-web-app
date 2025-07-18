29ee46e6a850ee070d093ec8345c8178a016f1ff
Anuda Ransara <anudaransara1@gmail.com>
2025-07-18 23:55:37 +0530

    create user register controller.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,63 @@
+import userModel from "../models/userModel.js";
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
+
+// Route for user loging--------------------------------------
+const loginUser = async (req, res) => {
+
+}
+
+// Route for ser register------------------------------------
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        // check user existing or not
+        const exist = await userModel.findOne({ email })
+        if (exist) {
+            return res.json({ success: false, message: "User alredy existes" })
+        }
+
+        // Validation email format-----------------------------
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Place enter a valid email" })
+        }
+        if (password.length < 8 ){
+             return res.json({ success: false, message: "Place enter strong password" })
+        }
+
+        // Hashing user password--------------------------------------
+        const salt = await bcrypt.genSalt(10) 
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const user = await newUser.save()
+
+        const token = createToken(user._id)
+        res.json({success:true, token})
+
+    }
+    catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+        
+    }
+}
+
+// Rute for admin login -------------------------------------
+const adminLogin = async (req, res) => {
+
+}
+
+export { loginUser, registerUser, adminLogin }
