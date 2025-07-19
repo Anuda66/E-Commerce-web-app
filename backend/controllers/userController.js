4b6d05d68d9751c772e27111d2e272df5e3292df
Anuda Ransara <anudaransara1@gmail.com>
2025-07-19 12:34:20 +0530

    create loging controller.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,14 +3,35 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET)
+    return jwt.sign({ id }, process.env.JWT_SECRET)
 }
 
 // Route for user loging--------------------------------------
 const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
+        const user = await userModel.findOne({ email })
+
+        if (!user) {
+            return res.json({ success: false, message: "User doesn't exists" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({ success: true, token })
+        }
+        else {
+            res.json({ success: false, message: 'Invalid credential' })
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // Route for ser register------------------------------------
@@ -28,12 +49,12 @@ const registerUser = async (req, res) => {
         if (!validator.isEmail(email)) {
             return res.json({ success: false, message: "Place enter a valid email" })
         }
-        if (password.length < 8 ){
-             return res.json({ success: false, message: "Place enter strong password" })
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Place enter strong password" })
         }
 
         // Hashing user password--------------------------------------
-        const salt = await bcrypt.genSalt(10) 
+        const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
 
         const newUser = new userModel({
@@ -45,13 +66,13 @@ const registerUser = async (req, res) => {
         const user = await newUser.save()
 
         const token = createToken(user._id)
-        res.json({success:true, token})
+        res.json({ success: true, token })
 
     }
     catch (error) {
         console.log(error);
-        res.json({success:false, message:error.message})
-        
+        res.json({ success: false, message: error.message })
+
     }
 }
 
