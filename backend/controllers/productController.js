59f62414518eb44e6a6092e435601495a398056f
Anuda Ransara <anudaransara1@gmail.com>
2025-07-20 23:26:38 +0530

    create peoduct router path.

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+//API for add product------------------------------
+const addProduct = async (req, res) => {
+
+}
+
+//API fro list product-----------------------------
+const listProduct = async (req, res) => {
+
+}
+
+//API fro remove product--------------------------
+const removeProduct = async (req, res) => {
+
+}
+
+//API fro single product info--------------------------
+const singleProduct = async (req, res) => {
+
+}
+
+export { addProduct, listProduct, removeProduct, singleProduct }
