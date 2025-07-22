e0570d6d39c0acd9de57c913db9dd4050931b533
Anuda Ransara <anudaransara1@gmail.com>
2025-07-22 15:27:24 +0530

    ERROR SOLE ADD PRODUCT.

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -3,9 +3,9 @@ import {v2 as cloudinary} from 'cloudinary'
 const connectCloudinary = async () => {
 
     cloudinary.config({
-        cloude_name: process.env.COLUDINARY_NAME,
+        cloud_name: process.env.COLUDINARY_NAME,
         api_key: process.env.COLUDINARY_API_KEY,
-        api_secret_key: process.env.COLUDINARY_SECRET_KEY
+        api_secret: process.env.COLUDINARY_SECRET_KEY
     })
 }
 
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,22 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary'
 
+
+cloudinary.config({
+        cloude_name: process.env.COLUDINARY_NAME,
+        api_key: process.env.COLUDINARY_API_KEY,
+        api_secret_key: process.env.COLUDINARY_SECRET_KEY
+    })
 
 //API for add product------------------------------
 const addProduct = async (req, res) => {
     try {
         const { name, description, price, category, subCategory, size, bestseller } = req.body
 
-         // Check if files exist
-        if (!req.files) {
-            return res.status(400).json({ success: false, message: "No files were uploaded" });
-        }
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
 
-        const image1 = req.files.image1[0]
-        const image2 = req.files.image2[0]
-        const image3 = req.files.image3[0] 
-        const image4 = req.files.image4[0]
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, { resource_type: 'image' })
+                return result.secure_url
+            })
+        )
 
         console.log(name, description, price, category, subCategory, size, bestseller)
-        console.log(image1, image2, image3, image4);
+        console.log(imagesUrl);
 
         res.json({})
 
@@ -32,7 +43,7 @@ const listProduct = async (req, res) => {
     try {
 
     }
-    catch (error) { 
+    catch (error) {
 
     }
 }
