cae0fca8c542c7ce64df044400bdc8eb3dfd8664
Anuda Ransara <anudaransara1@gmail.com>
2025-07-24 23:59:38 +0530

    admin app set up

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -7,12 +7,14 @@ const adminAuth = async (req, res, next) => {
             return res.json({success:false, message: 'Not authorized login provided'});
         }
         const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-        if (token_decode !== process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+        if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD){
             return res.json({success:false, message: 'Not authorized login provided'});
         }
         next();
     }
     catch (error){
+        console.log(error);
+        res.json({success: false, message: error.message});
         
     }
 }
