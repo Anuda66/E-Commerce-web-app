d13065e47b4d7fe989f7be5588cfa8ee7bc3caf1
Anuda Ransara <anudaransara1@gmail.com>
2025-07-25 23:13:37 +0530

    setup filie structure.

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+
+function Navbar() {
+  return (
+    <div>
+     <img src={assets.logo} alt="logo" />
+     <button></button>
+    </div>
+  )
+}
+
+export default Navbar
