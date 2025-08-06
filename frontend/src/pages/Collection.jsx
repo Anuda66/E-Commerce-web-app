c7a1a6f77b942896f11cfcf8c13d4bd6dbeffc16
Anuda Ransara <anudaransara1@gmail.com>
2025-08-07 00:34:47 +0530

    singel product intigation

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -74,7 +74,7 @@ function Collection() {
 
   useEffect(() => {
     applyFilter()
-  },[category, subCategory, search, showSearch])
+  },[category, subCategory, search, showSearch, products])
 
   // useEffect(() => {
   //   setFilterProducts(products)
@@ -147,7 +147,7 @@ function Collection() {
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
           {
             filterProducts.map((item, index) => (
-              <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+              <ProductItem key={index} id={item._id} images={item.images} name={item.name} price={item.price} />
             ))
           }
         </div>
